e96c7a3444976be3ddd13c7f602d4769fca39cc9
Ibrar Yunus - (Lenovo Y700) <iy3@st-andrews.ac.uk>
2018-07-05 10:20:06 +0100

    improved structure

--- src/Main.kt
+++ src/Main.kt
@@ -1,39 +1,18 @@
+import stack.Stack
+
 /* Kotlin Stacks
     Ibrar Yunus <University of St. Andrews>
 
-    Generic implementation of Stack
+    Generic implementation of stack.Stack
  */
 
-class Stack<E>{
-    private val stack = mutableListOf<E>()
-    val size get() = stack.size
-    val isEmpty get() = size == 0
-
-    fun pop(): E {
-        if (isEmpty) throw RuntimeException("stack empty - cannot pop")
-        return stack.removeAt(stack.lastIndex)
-    }
-
-    fun push(element: E) =  stack.add(element)
-
-    val top: E
-        get () {
-            if(isEmpty) throw RuntimeException("stack empty - has no top element")
-            return stack.last()
-        }
-
-    fun clear()   {
-        stack.clear()
-    }
-}
-
-fun main (args: Array<String>) {
+fun main(args: Array<String>) {
     header()
 
     println("Testing Stacks!")
     println("-- pushing into stack")
     val test = Stack<Int>()
-    (1..10).forEach{ test.push(it)}
+    (1..10).forEach { test.push(it) }
 
     println("-- checking size: " + test.size)
 
